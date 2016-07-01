5d1e97e972513be0518eeeb102cf4f8003d8f506
gizmore <gizmore@wechall.net>
2016-07-02 01:35:54 +0200

    xlin changes

--- lib/ricer4/plugins/core_shell/xlin.rb
+++ lib/ricer4/plugins/core_shell/xlin.rb
@@ -1,7 +1,7 @@
 module Ricer4::Plugins::Shell
   class Xlin < Ricer4::Plugin
     
-    has_setting name: :usergroups, type: :enum, default: :nom, values: [:om, :nom]
+    has_setting name: :usergroups, type: :enum, default: :nom, values: [:om, :nom], scope: :bot
     
     trigger_is "xlin"
     has_usage "<username> <password>"
