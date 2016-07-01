0f1399fafffc47838c90ac6ea4fd85b31ee8eba5
gizmore <gizmore@wechall.net>
2016-07-02 01:36:05 +0200

    Better test

--- spec/ricer4/01_base_spec.rb
+++ spec/ricer4/01_base_spec.rb
@@ -1,19 +1,27 @@
 require 'spec_helper'
 describe Ricer4::Bot do
   
+  USERS = Ricer4::User
+  SERVERS = Ricer4::Server
+  CHANNELS = Ricer4::Channel
+
+  bot = Ricer4::Bot.new("ricer4.spec.conf.yml")
+  bot.db_connect
+
   it("loads the plugins and can exec echo") do
-    
-    # LOAD
-    bot = Ricer4::Bot.new("ricer4.spec.conf.yml")
-    bot.db_connect
     ActiveRecord::Magic::Update.install
     ActiveRecord::Magic::Update.run
     bot.load_plugins
     ActiveRecord::Magic::Update.run
-
-    # Test cases
+  end
+  
+  it("can truncate all tables") do
+    USERS.destroy_all
+    CHANNELS.destroy_all
+  end
+  
+  it("can execute the echo command") do
     expect(bot.exec_line("echo 4 + 2")).to eq("4 + 2")
-
   end
   
 end
